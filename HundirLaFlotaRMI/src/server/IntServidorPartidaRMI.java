3e033ce99696076e0ba40bed631ceb42aedd3891
DavidCatalan <freedavid94@gmail.com>
2014-11-06 13:55:22 +0100

    Interfaz del servidor del objeto remoto.

--- HundirLaFlotaRMI/src/server/IntServidorPartidaRMI.java
+++ HundirLaFlotaRMI/src/server/IntServidorPartidaRMI.java
@@ -1,7 +1,14 @@
 package server;
 
 import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+import common.Partida;
 
 public interface IntServidorPartidaRMI extends Remote {
-
+	public Partida nuevaPartida(int numFilas, int numCols, int numBarcos) throws RemoteException;
+	public int pruebaCasilla(int numFil, int numCol) throws RemoteException;
+	public String getBarco(int id) throws RemoteException;
+	public String[] getSolucion() throws RemoteException;
+	
 }
